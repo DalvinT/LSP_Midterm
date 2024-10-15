75e94d10bf6f61223f0a300339e53e5424952812
Dalvin Ticha <dalvinticha@Dalvins-MacBook-Pro.local>
2024-10-15 00:02:37 -0400

    Added HelloWorld.java files to question1, question2, question3, and question4

---
+++ src/org/howard/edu/lsp/midterm/question1/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.howard.edu.lsp.midterm.questionX;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from question1!");
+    }
+}
+
